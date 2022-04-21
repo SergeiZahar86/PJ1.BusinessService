8ddbc705643c26a2c3f02eec0f5588c2b0d6ad10
Сергей Захаров <sergei86zahar@mail.ru>
2022-04-21 16:14:04 +0200

    Initial commit

---
+++ WorkTime.BoardRecords.Web/Startup.cs
@@ -0,0 +1,152 @@
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System;
+using System.Collections.Generic;
+using WorkTime.BoardRecords.Web.models;
+
+namespace WorkTime.BoardRecords.Web
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(config =>
+            {
+                config.UseSqlServer(Configuration.GetConnectionString(nameof(ApplicationDbContext)));
+            })
+                .AddIdentity<AppUser, AppRole>(config =>
+                {
+                    config.Password.RequireDigit = false;
+                    config.Password.RequireLowercase = false;
+                    config.Password.RequireNonAlphanumeric = false;
+                    config.Password.RequireUppercase = false;
+                    config.Password.RequiredLength = 4;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+            services.AddCors(config =>
+            {
+                config.AddPolicy("DefaultPolicy",
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Description = "Demo Swagger API v1",
+                    Title = "Swagger with IdentityServer4",
+                    Version = "1.0.0"
+                });
+
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        Password = new OpenApiOAuthFlow
+                        {
+                            TokenUrl = new Uri("https://localhost:10001/connect/token"),
+                            Scopes = new Dictionary<string, string>
+                            {
+                                //{"SwaggerAPI", "Swagger API DEMO"}
+                            }
+                        }
+                    }
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "oauth2"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer("Bearer",
+                     options =>
+                     {
+                         options.Authority = "https://localhost:10001";
+                         options.Audience = "SwaggerAPI";
+                         options.RequireHttpsMetadata = false;
+
+                         options.TokenValidationParameters = new TokenValidationParameters()
+                         {
+                             ValidateAudience = false
+                         };
+                     });
+
+            services.AddAuthorization();
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling =
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+            services.AddMvc();
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger UI Demo");
+                options.DocumentTitle = "Title";
+                options.RoutePrefix = "docs";
+                options.DocExpansion(DocExpansion.List);
+                options.OAuthClientId("client_business");
+                options.OAuthScopeSeparator(" ");
+                options.OAuthClientSecret("client_secret_swagger");
+            });
+
+            app.UseRouting();
+            app.UseCors("DefaultPolicy");
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
